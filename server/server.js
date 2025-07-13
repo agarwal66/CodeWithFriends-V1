d28a381486f9e298a45a1cead66b4877c9f2c345
Prateek Agarwal <agarwalprateek666@gmail.com>
2025-07-13 19:08:58 +0530

    update

--- server/server.js
+++ server/server.js
@@ -156,9 +156,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    // secure: true,
+    secure: true,
    secure:process.env.NODE_ENV==="production",
-    // httpOnly: true,
+    httpOnly: true,
      
     sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000 // 1 day
